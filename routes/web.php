2f4a46dff4ef049aa42a763e9fd1dbf156300cb6
MaicolDdox <maicolduvangascarodas@gmail.com>
2026-02-04 20:08:29 -0500

    implemente routes

--- routes/web.php
+++ routes/web.php
@@ -1,13 +1,40 @@
 <?php
 
+use App\Http\Controllers\Admin\DriverController;
+use App\Http\Controllers\Admin\OwnerController;
+use App\Http\Controllers\Admin\VehicleController;
+use App\Http\Controllers\Admin\VehicleReportController;
 use Illuminate\Support\Facades\Route;
 
 Route::get('/', function () {
     return view('welcome');
 })->name('home');
 
-Route::view('dashboard', 'dashboard')
-    ->middleware(['auth', 'verified'])
-    ->name('dashboard');
+Route::middleware(['auth', 'verified'])->group(function () {
+    Route::view('dashboard', 'dashboard')->name('dashboard');
+
+    Route::prefix('admin')->group(function () {
+        Route::resource('conductores', DriverController::class)
+            ->names('admin.drivers')
+            ->parameters(['conductores' => 'driver'])
+            ->except(['show']);
+
+        Route::resource('propietarios', OwnerController::class)
+            ->names('admin.owners')
+            ->parameters(['propietarios' => 'owner'])
+            ->except(['show']);
+
+        Route::resource('vehiculos', VehicleController::class)
+            ->names('admin.vehicles')
+            ->parameters(['vehiculos' => 'vehicle'])
+            ->except(['show']);
+
+        Route::get('informe/vehiculos', [VehicleReportController::class, 'index'])
+            ->name('admin.reports.vehicles.index');
+
+        Route::get('informe/vehiculos/pdf', [VehicleReportController::class, 'pdf'])
+            ->name('admin.reports.vehicles.pdf');
+    });
+});
 
 require __DIR__.'/settings.php';
